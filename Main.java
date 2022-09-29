6a314cde95a06a95b9e3045e203b9b4449f37ca5
lionhard83 <carloleonardi83@gmail.com>
2022-09-29 11:53:37 +0200

    Add black jack

--- Main.java
+++ Main.java
@@ -1,22 +1,29 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // SicilianDeck sd = new SicilianDeck();
-        // sd.shuffle();
-        // int i = 0;
-        // while (sd.hasHead()) {
-        // i++;
-        // System.out.println(sd.getHead());
-        // }
-        // System.out.println(i);
 
-        FrenchDeck fd = new FrenchDeck(2);
-        fd.shuffle();
-        int i = 0;
-        while (fd.hasHead()) {
-            i++;
-            System.out.println(fd.getHead());
+        BlackJack b = new BlackJack();
+        b.start();
+        Scanner s = new Scanner(System.in);
+        System.out.println(b);
+        System.out.println("Chiami (1) o Stai (0)");
+        int call = s.nextInt();
+        while (call == 1) {
+            b.call();
+            System.out.println(b);
+            if (b.current.isStone() || b.current.getScore().get(b.current.getScore().size() - 1) == 21) {
+                break;
+            }
+            System.out.println("Chiami (1) o Stai (0)");
+            call = s.nextInt();
         }
-        System.out.println(i);
+        System.out.println(b);
+        b.playDealer();
+        System.out.println(b);
+        System.out.println("The winner is :" + b.getWinner());
+
+        // System.out.println(p.getScore());
 
     }
 }
